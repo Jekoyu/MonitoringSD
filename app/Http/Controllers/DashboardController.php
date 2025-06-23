29f50b910e32284e2d917049dde25ac99a3b4507
Jekoyu <jekoyu157@gmail.com>
2025-06-24 01:10:37 +0700

    fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,20 +13,8 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // $identity = Storage::get('mikrotik/resource.json'); 
-        // $data = json_decode($identity, true);
-        // $systemInfo = $data[0];
+        
     
-        // $uptime = $systemInfo['uptime'];
-
-        // $json = Storage::get('mikrotik/speed.json');
-        // $data = json_decode($json, true);
-    
-        // $ether2 = collect($data)->firstWhere('name', 'ether2');
-    
-        return view('home', [
-            // 'ether2' => $ether2,
-            // 'uptime' => $uptime,
-        ]);
+        return view('home');
     }
 }
