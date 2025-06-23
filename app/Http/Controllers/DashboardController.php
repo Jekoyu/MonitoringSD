1890b52524e972e60582e257120859b94b6764f7
Jekoyu <jekoyu157@gmail.com>
2025-06-24 01:12:26 +0700

    fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,19 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Device;
-use App\Models\NetworkLog;
-use App\Models\Notification;
-use Illuminate\Http\Request;
-use App\Models\BandwidthUsage;
-use Illuminate\Support\Facades\Storage;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        
-    
         return view('home');
     }
 }
